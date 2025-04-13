c0700339ddc68e3cc2a35657c51ebf5632dd38fd
nagigithup <anagii66@gmail.com>
2025-04-13 23:46:20 +0300

    new update

---
+++ spf/spf/doctype/businessman/businessman.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Nagi and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Businessman(Document):
+	pass
